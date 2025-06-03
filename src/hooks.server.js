425cb400dcb0f2e0f5b28a74659a319f9266d0c6
marcegolf <marc.egolf@outlook.com>
2025-06-03 13:53:34 +0200

    feat: write locals based on authentication state
    Relates: #5

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,5 +2,18 @@ import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
-    return svelteKitHandler({ event, resolve, auth });
+    const fetchedSession = await auth.api.getSession({
+        headers: event.request.headers
+    });
+
+    if (fetchedSession) {
+        const { user, session } = fetchedSession;
+        event.locals.session = session;
+        event.locals.user = user;
+    } else {
+        delete event.locals.session;
+        delete event.locals.user;
+    }
+    
+    return await svelteKitHandler({ event, resolve, auth });
 }
