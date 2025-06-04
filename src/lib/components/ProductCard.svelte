94a64e663e52f0f1521b03da9174cbd3f3e172c6
marcegolf <marc.egolf@outlook.com>
2025-06-04 10:03:01 +0200

    refactor: optimized styling of product card
    Relates: #3

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -4,31 +4,24 @@
   let { product } = $props();
 </script>
 
-<div class="card h-100 bg-dark text-white">
-  <img src={product.poster} class="card-img-top" alt={product.name} />
+<div class="d-flex justify-content-center">
+  <!-- <div class="card h-100 bg-transparent text-white" style="max-width: 18rem;"> -->
+   <div class="card h-100 bg-transparent text-white">
+    <img src={product.poster} class="card-img-top" alt={product.name} />
 
-  <div class="card-body d-flex flex-column">
-    <h5 class="card-title">
+    <div class="card-body d-flex flex-column align-items-center text-center">
+      <h5 class="card-title">
       <a
         href={"/products/" + product._id}
         class="text-white text-decoration-none"
       >
         {product.name}
       </a>
-    </h5>
+      </h5>
 
-    <p class="card-text mb-2">Preis: {product.price}</p>
+      <p class="card-text mb-2">CHF {product.price}</p>
 
-    <!-- <strong>Ingredients:</strong>
-    <ul class="list-group list-group-flush mb-3">
-      {#each product.ingredients as ingredient}
-        <li class="list-group-item bg-dark border-dark text-white">
-          {ingredient}
-        </li>
-      {/each}
-    </ul> -->
-
-    <form method="POST" action="?/addToCart" use:enhance>
+      <form method="POST" action="?/addToCart" use:enhance>
       <input
         type="hidden"
         name="product"
@@ -37,6 +30,7 @@
       <button class="btn btn-primary mt-auto">
         In den Warenkorb
       </button>
-    </form>
+      </form>
+    </div>
   </div>
 </div>
